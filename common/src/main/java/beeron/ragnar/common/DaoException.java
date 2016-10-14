9fc5376a3be2eea351e7b38665054e7823a0dcf0
Beeron <tichrules@gmail.com>
2016-10-14 23:11:00 +0200

    gifsdfsd f

---
+++ common/src/main/java/beeron/ragnar/common/DaoException.java
@@ -0,0 +1,10 @@
+package beeron.ragnar.common;
+
+public class DaoException extends Exception {
+
+	private static final long serialVersionUID = -1762836388709090353L;
+
+	public DaoException(Throwable e) {
+		super(e);
+	}
+}
---
+++ webapp/src/main/webapp/WEB-INF/pages/error.jsp
@@ -0,0 +1,5 @@
+<html>
+<body bgcolor="white">
+	<p>Error: ${message}</p>
+</body>
+</html>
